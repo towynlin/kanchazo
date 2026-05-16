dd40092edc619e64a35bdf889c51da97851a169e
Zachary Crockett <zachary.crockett@hey.com>
2026-05-15 20:22:57 -0700

    prettier

--- dbsetup.js
+++ dbsetup.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-const { spawn } = require('node:child_process')
+const { spawn } = require("node:child_process");
 
-const env = { ...process.env }
+const env = { ...process.env };
 
-;(async() => {
-  await exec('npx next build --experimental-build-mode generate')
+(async () => {
+  await exec("npx next build --experimental-build-mode generate");
 
   // launch application
-  await exec(process.argv.slice(2).join(' '))
-})()
+  await exec(process.argv.slice(2).join(" "));
+})();
 
 function exec(command) {
-  const child = spawn(command, { shell: true, stdio: 'inherit', env })
+  const child = spawn(command, { shell: true, stdio: "inherit", env });
   return new Promise((resolve, reject) => {
-    child.on('exit', code => {
+    child.on("exit", (code) => {
       if (code === 0) {
-        resolve()
+        resolve();
       } else {
-        reject(new Error(`${command} failed rc=${code}`))
+        reject(new Error(`${command} failed rc=${code}`));
       }
-    })
-  })
+    });
+  });
 }
